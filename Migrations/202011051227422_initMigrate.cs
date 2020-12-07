d46368657617474b492aaf1085e3379f76a596ce
Qlity Gigs <54314059+Tech-Squads@users.noreply.github.com>
2020-12-07 10:57:57 +0200

    Deployed  API

--- Migrations/202011051227422_initMigrate.cs
+++ Migrations/202011051227422_initMigrate.cs
@@ -18,12 +18,33 @@
                         FirstLogin = c.String(),
                     })
                 .PrimaryKey(t => t.UserID);
-            
+
+            CreateTable(
+                "dbo.Profiles",
+                c => new
+                {
+                    ProfileID = c.Int(nullable: false, identity: true),
+                    uName = c.String(),
+                    uSurname = c.String(),
+                    uCountry = c.String(),
+                    uCompany = c.String(),
+                    uEducation = c.String(),
+                    uSkills = c.String(),
+                    uReferences = c.String(),
+                    PastProjectName = c.String(),
+                    PastProjectDuration = c.String(),
+                    PastProjectDetails = c.String(),
+                    UserID = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.ProfileID);
+
+
         }
-        
+
         public override void Down()
         {
             DropTable("dbo.Users");
+            DropTable("dbo.Profiles");
         }
     }
 }
